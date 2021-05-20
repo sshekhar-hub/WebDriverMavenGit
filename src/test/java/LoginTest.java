7207471c5b2052b6467a12c4c874bcf15c48c78b
sshekhar <shekhar.swarna@gmail.com>
2021-05-20 17:17:36 +0530

    Initial Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public WebDriver driver;
+	@BeforeSuite
+	public void setup() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("shekhar.swarna@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("goodguy@6682");
+		driver.findElement(By.name("login")).click();
+		
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+}
